97ce1fabd52a38a4860c74463f858a401cce2446
Bruna Eurich <schinemann.bruna@escola.pr.gov.br>
2026-06-11 11:24:13 -0300

    sim

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-```javascript
+// Botão da mensagem
 const botao = document.getElementById("btnMensagem");
 
-botao.addEventListener("click", () => {
-    alert("🌱 Agro Forte, Futuro Sustentável!");
-});
-```
+if (botao) {
+    botao.addEventListener("click", () => {
+        alert("🌱 Agro Forte, Futuro Sustentável!");
+    });
+}
+
+// Acessibilidade - Controle de fonte
+let tamanhoFonte = 100;
+
+function aumentarFonte() {
+    tamanhoFonte += 10;
+    document.body.style.fontSize = tamanhoFonte + "%";
+}
+
+function diminuirFonte() {
+    if (tamanhoFonte > 70) {
+        tamanhoFonte -= 10;
+        document.body.style.fontSize = tamanhoFonte + "%";
+    }
+}
